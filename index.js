4b96b569c70a8c217552637a355718b5aebad06a
David Cha <david.cha44@qmail.cuny.edu>
2019-10-11 23:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function sayHiToGrandma(string) {
     var canHear = 'I can hear you!'
       var loveYou = 'I love you too!'
           if(string.toLowerCase === string){
-          return cantHear
+          return cantHear;
           }
             else if(string.toUpperCase === string){
-              return canHear
+              return canHear;
             }
                 else if('I love you Grandma' === string) {
-                  return loveYou
+                  return loveYou;
                 }
 }
